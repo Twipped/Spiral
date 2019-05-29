672781855e7f95cc5e3a509dc470728388b53708
Jocelyn Badgley (Twipped) <joc@twipped.com>
2019-05-28 17:40:32 -0700

    Switching asyncstorage to external package

--- android/app/src/main/java/com/spiral/MainApplication.java
+++ android/app/src/main/java/com/spiral/MainApplication.java
@@ -3,6 +3,7 @@ package com.spiral;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage()
--- stores/CalendarStore/index.js
+++ stores/CalendarStore/index.js
@@ -1,5 +1,5 @@
 
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { action, observable, reaction, computed } from 'mobx';
 import { Month } from './classes';
 import Promise from 'bluebird';
